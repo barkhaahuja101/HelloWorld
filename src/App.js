9091a524f6609c8dba4b83d7c0e97df3c01470e9
barkhaahuja101 <barkhaahuja279@gmail.com>
2024-06-12 19:35:41 +0530

    simple react application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+function ClickableButton() {
+  const [message, setMessage] = useState('');
+
+  const handleClick = () => {
+    axios.get('http://localhost:8080/hello')
+      .then(response => {
+        setMessage(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+      });
+  };
+
+
+return (
+  <div>
+    <button onClick={handleClick}>Click Me</button>
+    <p>{message}</p>
+  </div>
+);
+
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Click the Button</h3>
+      <ClickableButton />
     </div>
   );
 }
